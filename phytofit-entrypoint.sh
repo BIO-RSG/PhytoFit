34536710638df670cddd583fe953afef69f590ce
Jeff Cullis <jcullis@dal.ca>
2022-03-30 15:06:54 -0300

    #3 Pipe yes to all dataset downloads.

--- phytofit-entrypoint.sh
+++ phytofit-entrypoint.sh
@@ -2,6 +2,6 @@
 # Link in existing datasets
 ln -s /data /PhytoFit/data
 # Download any new datasets
-Rscript download_new_datasets.R
+yes | Rscript download_new_datasets.R
 # Run the app
 R -e "shiny::runApp('app.R', port = 7520, host = '0.0.0.0')"
